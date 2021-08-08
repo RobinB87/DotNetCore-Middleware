9ce8f14ced4ca369874aea8922d5e14f65f74ad9
Robin Bette <robinbette@hotmail.com>
2021-08-08 11:41:49 +0200

    Update middleware to use appsettings

--- MyCustomMiddleware.cs
+++ MyCustomMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 
 namespace DotNetMiddleware
@@ -9,23 +10,32 @@ namespace DotNetMiddleware
     {
         RequestDelegate _next;
         ILoggerFactory _loggerFactory;
+        IOptions<MyCustomMiddlewareOptions> _options;
 
-        public MyCustomMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        public MyCustomMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IOptions<MyCustomMiddlewareOptions> options)
         {
             _next = next;
             _loggerFactory = loggerFactory;
+            _options = options;
         }
 
         public async Task Invoke(HttpContext context)
         {
-            _loggerFactory.AddConsole();
-            var logger = _loggerFactory.CreateLogger("My own logger..");
-            logger.LogInformation("My middleware class is handling the request");
+            //_loggerFactory.AddConsole();
+            //var logger = _loggerFactory.CreateLogger("My own logger..");
+            //logger.LogInformation("My middleware class is handling the request");
 
-            await context.Response.WriteAsync("My middleware class is handling the request");
+            //await context.Response.WriteAsync("My middleware class is handling the request");
+            await context.Response.WriteAsync(_options.Value.OptionOne + "<br>");
+
             await _next.Invoke(context);
             await context.Response.WriteAsync("My middleware class has completed handling the request");
         }
+    }
+
+    public class MyCustomMiddlewareOptions
+    {
+        public string OptionOne { get; set; }
     }
 
     public static class MyCustomMiddlewareExtensions
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -8,10 +9,26 @@ namespace DotNetMiddleware
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true);
+
+            Configuration = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+
+            var myOptions = Configuration.GetSection("MyMiddlewareOptionsSection");
+            services.Configure<MyCustomMiddlewareOptions>(o => o.OptionOne = myOptions["OptionOne"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
